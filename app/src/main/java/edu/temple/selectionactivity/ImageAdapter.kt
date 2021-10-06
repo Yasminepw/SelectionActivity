e5af9fee2adcb62d0464ecd2f55b1bd89cd5f429
Yasminepw <yasminepwatkins@gmail.com>
2021-10-06 12:19:25 -0400

    Updated all the layouts

---
+++ app/src/main/java/edu/temple/selectionactivity/ImageAdapter.kt
@@ -0,0 +1,10 @@
+package edu.temple.selectionactivity
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageAdapter {
+}
